a4aa893a5a335257e486abd562fc2cb84845c318
harineko0 <harineko0927@gmail.com>
2024-06-14 23:59:58 +0900

    fix .gitignore

--- gas/dist/main.js
+++
@@ -1,4 +0,0 @@
-let global = this;
-function sample() {
-}
-"use strict";(()=>{global.sample=()=>{console.log("---sample---")};})();
--- gas/src/index.ts
+++ gas/src/index.ts
@@ -1,3 +1,3 @@
-function helloWorld() {
+global.helloWorld = () => {
     console.log('Hello, World!');
 }
