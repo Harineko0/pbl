048110b95008b7701272a18268282674ffb8040a
harineko0 <harineko0927@gmail.com>
2024-06-28 15:30:10 +0900

    ADD: added entities

---
+++ gas/src/db/entity/shift.ts
@@ -0,0 +1,6 @@
+export interface Shift {
+    id: string;
+    date: Date;
+    shift_type: string;
+    worker_id: string;
+}
---
+++ gas/src/db/entity/swap.ts
@@ -0,0 +1,5 @@
+export interface Swap {
+    id: string;
+    old_shift_id: string;
+    new_shift_id: string;
+}
