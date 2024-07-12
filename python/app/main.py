bda6366e9ade6a4d67d2c64349d5221f84004edf
harineko0 <harineko0927@gmail.com>
2024-07-12 18:23:08 +0900

    FIX: fix api response type

--- python/app/main.py
+++ python/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from request import Request
 from makeshift import make_shift
+import json
 
 app = FastAPI()
 
@@ -8,4 +9,4 @@ app = FastAPI()
 @app.post("/")
 async def root(req: Request):
     shift = make_shift(req)
-    return shift
+    return json.loads(shift)
