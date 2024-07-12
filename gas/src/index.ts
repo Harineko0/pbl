da25286a24e4eae49b439dcf6bd9d09f3ca4cff6
harineko0 <harineko0927@gmail.com>
2024-07-12 16:34:07 +0900

    ADD: added global.createShiftButton

--- gas/src/index.ts
+++ gas/src/index.ts
@@ -2,6 +2,7 @@ import {doGet} from "./http/doGet";
 import {onEdit} from "./onEdit";
 import {resetCheckbox} from "./edit_handlers/sendAttendanceEmailHandler";
 import {swapButton} from "./swapButton";
+import {createShiftButton} from "./createShiftButton";
 
 global.main = () => {
     console.log('Hello World!');
@@ -11,3 +12,4 @@ global.doGet = doGet;
 global.onEdit = onEdit;
 global.resetCheckbox = resetCheckbox;
 global.swapButton = swapButton;
+global.createShiftButton = createShiftButton;
