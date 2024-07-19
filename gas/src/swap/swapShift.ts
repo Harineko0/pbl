5c1d6fc161b4f136f48751b6a73511c04515535d
harineko0 <harineko0927@gmail.com>
2024-07-19 14:51:58 +0900

    FIX: swap

--- gas/src/swap/swapShift.ts
+++ gas/src/swap/swapShift.ts
@@ -1,39 +1,39 @@
-export function shiftswap(id: any) {
-  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet1 = spreadsheet.getSheetByName("shifts");
-  const sheet2 = spreadsheet.getSheetByName("swaps");
-  if (sheet1==null||sheet2==null){
-    return;
-  }
-  const lastRow1 = sheet1.getLastRow();
-  const lastRow2 = sheet2.getLastRow();
-  let range_a;
-  let range_b;
-  let list;
-  for (let k=1;k<lastRow2;k++){
-    var num = sheet2.getRange(k+1,1).getValue()
-    if(num == id){
-      list = sheet2.getRange(k+1,2,1,2).getValues();
-      break;
+export function shiftswap(id: string) {
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const shiftSheet = spreadsheet.getSheetByName("shifts");
+    const swapSheet = spreadsheet.getSheetByName("swaps");
+    if (shiftSheet == null || swapSheet == null) {
+        return;
     }
-  }
-  if(list == null){
-    return;
-  }
-  for (let j=0;j<lastRow1-1;j++){
-    var b =sheet1.getRange(j+2,1).getValue();
-    if(sheet1.getRange(j+2,1).getValue() == list[0][0]){
-      range_a = sheet1.getRange(j+2,4);
+    const shiftLastRow = shiftSheet.getLastRow();
+    const swapLastRow2 = swapSheet.getLastRow();
+    let swap = null;
+    for (let i = 1; i < swapLastRow2; i++) {
+        const swapId = swapSheet.getRange(i + 1, 1).getValue()
+        if (swapId == id) {
+            swap = swapSheet.getRange(i + 1, 2, 1, 2).getValues();
+            break;
+        }
     }
-    else if(sheet1.getRange(j+2,1).getValue() == list[0][1]){
-      range_b = sheet1.getRange(j+2,4);
+    if (swap == null) {
+        return;
     }
-  }
-  if(range_a==null||range_b==null){
-    return;
-  }
-  let range_values_a = range_a.getValues(); 
-  let range_values_b = range_b.getValues(); 
-  range_a.setValues(range_values_b);
-  range_b.setValues(range_values_a);
+    let targetShift: GoogleAppsScript.Spreadsheet.Range | null = null;
+    let srcShift: GoogleAppsScript.Spreadsheet.Range | null = null;
+
+    for (let i = 0; i < shiftLastRow - 1; i++) {
+        const shiftId = shiftSheet.getRange(i + 2, 1).getValue();
+        if (shiftId == swap[0][0]) {
+            targetShift = shiftSheet.getRange(i + 2, 4);
+        } else if (shiftId == swap[0][1]) {
+            srcShift = shiftSheet.getRange(i + 2, 4);
+        }
+    }
+    if (targetShift == null || srcShift == null) {
+        return;
+    }
+    let targetValues = targetShift.getValues();
+    let srcValues = srcShift.getValues();
+    targetShift.setValues(srcValues);
+    srcShift.setValues(targetValues);
 }
